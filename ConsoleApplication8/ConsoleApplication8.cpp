859203c8057b24a2c51caa89f70ee6f200c6e046
Evgzek <zekamusijhcuk@gmail.com>
2023-04-20 22:10:24 +1000

    Добавьте файлы проекта.

---
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    // Проверяем, находятся ли клетки на одной диагонали
+    if (abs(x2 - x1) == abs(y2 - y1)) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
+
